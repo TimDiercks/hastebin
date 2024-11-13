8fba4aad30c5af6cb76ae21ddb9987dad3fa3bfe
Tim Diercks <tim.r.diercks@gmail.com>
2024-11-13 21:40:59 +0100

    Remove automatic haste deletion

--- src/lib/server/config.ts
+++ src/lib/server/config.ts
@@ -1,4 +1,1 @@
-import { millisPerDay } from '$lib/unitConversions';
-
-export const HASTE_LIFETIME = millisPerDay * 7;
 export const MAX_TEXT_LENGTH = 4096;
--- src/lib/unitConversions.ts
+++
@@ -1,9 +0,0 @@
-export const millisPerSecond = 1000;
-export const secondsPerMinute = 60;
-export const minutesPerHour = 60;
-export const hoursPerDay = 24;
-
-export const secondsPerHour = secondsPerMinute * minutesPerHour;
-export const secondsPerDay = secondsPerHour * hoursPerDay;
-export const millisPerHour = millisPerSecond * secondsPerHour;
-export const millisPerDay = millisPerHour * hoursPerDay;
