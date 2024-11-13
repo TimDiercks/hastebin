207dd3d67da5967ce33e4003d90cb0b4639ca120
Tim Diercks <tim.r.diercks@gmail.com>
2024-11-13 21:38:42 +0100

    Put hastebin link into clipboard

--- src/lib/navbarCallbacks.ts
+++ src/lib/navbarCallbacks.ts
@@ -22,6 +22,7 @@ export const callbackSaveHaste = async () => {
 	const result = await response.json();
 
 	if (result.success) {
+		navigator.clipboard.writeText(get(page).url.href + `${result.slug}`);
 		goto(`/${result.slug}`);
 	}
 };
