fdc27544ae063d926c8ac69c2a29fc39a363d71a
Tim Diercks <tim.r.diercks@gmail.com>
2024-11-13 22:42:13 +0100

    Add code highlighting

--- src/routes/[slug]/+page.svelte
+++ src/routes/[slug]/+page.svelte
@@ -1,17 +1,23 @@
 <script lang="ts">
 	import type { PageData } from './$types';
 	import { viewHasteStore } from '../../stores';
+	import Highlight, { LineNumbers } from 'svelte-highlight';
+	import typescript from 'svelte-highlight/languages/typescript';
 
 	let { data }: { data: PageData } = $props();
 	$viewHasteStore = data.content;
 </script>
 
 <svelte:head>
-	<meta property="og:site_name" content={`HasteTim - ${data.slug}`}>
+	<meta property="og:site_name" content={`HasteTim - ${data.slug}`} />
 	<meta property="og:title" content={`HasteTim - ${data.slug}`} />
 	<meta property="og:description" content={`${data.content.substring(0, 200)}`} />
 </svelte:head>
 
 <div class="p-4 pt-28 md:pt-4">
-	<pre class="rounded-xl bg-slate-700 p-4 text-slate-300"><code class="text-wrap">{data.content}</code></pre>
+	<div class="overflow-hidden rounded-xl">
+		<Highlight language={typescript} code={data.content} let:highlighted>
+			<LineNumbers {highlighted} wrapLines hideBorder />
+		</Highlight>
+	</div>
 </div>
